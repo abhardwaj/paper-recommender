6f5e12f9de70c70033c0e840a5c32b991e96b9f9
Anant Bhardwaj <anantb@csail.mit.edu>
2013-03-13 01:03:43 -0400

    final stuff

--- lenskit-chi13/src/edu/mit/csail/Predictor.java
+++ lenskit-chi13/src/edu/mit/csail/Predictor.java
@@ -19,6 +19,7 @@ import org.grouplens.lenskit.knn.item.ItemItemRecommender;
 import org.grouplens.lenskit.knn.item.ItemItemScorer;
 import org.grouplens.lenskit.knn.item.NeighborhoodScorer;
 import org.grouplens.lenskit.knn.item.SimilaritySumNeighborhoodScorer;
+import org.grouplens.lenskit.knn.item.WeightedAverageNeighborhoodScorer;
 import org.grouplens.lenskit.knn.params.NeighborhoodSize;
 import org.grouplens.lenskit.knn.user.SimpleNeighborhoodFinder;
 import org.grouplens.lenskit.transform.normalize.BaselineSubtractingUserVectorNormalizer;
@@ -37,25 +38,20 @@ public class Predictor {
 			factory.bind(BaselinePredictor.class)
 			       .to(ItemUserMeanPredictor.class);
 			factory.bind(RatingPredictor.class).to(ItemItemRatingPredictor.class);
-			factory.set(NeighborhoodSize.class).to(50);
-			//factory.bind(NeighborhoodScorer.class).to(ItemItemScorer.class);
+			factory.set(NeighborhoodSize.class).to(30);
 			factory.bind(NeighborhoodScorer.class).to(SimilaritySumNeighborhoodScorer.class);
+			//factory.bind(NeighborhoodScorer.class).to(WeightedAverageNeighborhoodScorer.class);
 			factory.bind(ItemRecommender.class).to(ItemItemRecommender.class);
 			RecommenderEngine engine = factory.create();
 			/* get the and use the recommender */
 			Recommender rec = engine.open();
-			//ItemScorer r = rec.getItemScorer();
-			//double score = r.score(1, 10);
-			//System.out.println(score);
 			ItemRecommender irec = rec.getItemRecommender();
-			/* get recommendations from irec, or use e.g. getRatingPredictor() */
-			ScoredLongList recommendations = irec.recommend(1);
+			ScoredLongList recommendations = irec.recommend(16);
 			long items[] = new long[10];
 			double scores[] = new double[10];
 			recommendations.getElements(0, items, scores, 0, 10);
 			for(int i=0; i< 10; i++){
 				System.out.println(items[i] + ":" + scores[i]);
-
 			}
 			
 		} catch (RecommenderBuildException e) {
