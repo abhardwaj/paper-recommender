841ece7a0b58de31129f62a220509414af33362f
Anant Bhardwaj <anantb@csail.mit.edu>
2013-03-26 15:38:43 -0400

    mysql integration

---
+++ recommender/paper_db.py
@@ -0,0 +1,29 @@
+import os, re, sys
+import MySQLdb
+
+class AuthorSourceDatabase:
+
+	def __init__(self):
+		self.conn = MySQLdb.connect(host="mysql.csail.mit.edu", user="cobi", passwd="su4Biha", db="cobiDev")
+		self.cursor = self.conn.cursor()
+		print (self.conn)
+
+
+	def get_all(self):
+		self.cursor.execute("""SELECT authorId , interested FROM authorsourcing;""")
+		data = self.cursor.fetchall()
+		if data == None:
+		       	return None
+		for row in data:
+			print row
+
+	def get_author_data(self, id):
+		self.cursor.execute("select authors, title from entity where id='%s'" %(id))
+		data = self.cursor.fetchall()
+		if data == None:
+		       	return None
+		for row in data:
+			print row
+
+adb = AuthorSourceDatabase()
+adb.get_author_data('pn1566')
