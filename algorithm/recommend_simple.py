78bd5177cf1879e35e5fa1178eb2a9d39ad21467
Anant Bhardwaj <anantb@csail.mit.edu>
2013-03-27 10:29:10 -0400

    use grouplens always

--- algorithm/recommend_simple.py
+++ algorithm/recommend_simple.py
@@ -1,4 +1,3 @@
-from math import sqrt
 import sys, os, operator, json
 
 if __name__ == "__main__":
@@ -9,193 +8,21 @@ if __name__ == "__main__":
 
 from db.entity import *;
 from db.prefs import *
+from py4j.java_gateway import JavaGateway
 
-
-'''
-returns a distance-base similarity score for person1 and person2
-'''
-def sim_ecld(prefs, p1, p2):
-	# get the list of shared_items
-	si = {}
-	for item in prefs[p1]:
-		if item in prefs[p2]:
-			si[item] = 1
-
-	# if they have no rating in common, return 0
-	if len(si) == 0: 
-		return 0
-
-	# add up the squares of all differences
-	d = sqrt(sum([pow((prefs[p1][item]-prefs[p2][item]),2) for item in prefs[p1] if item in prefs[p2]]))
-
-	return 1 / (1 + d)
-
-
-
-
-'''
-returns the Pearson correlation coefficient for p1 and p2 
-'''
-def sim_pearson(prefs,p1,p2):
-	# get the list of mutually rated items
-	si = {}
-	for item in prefs[p1]:
-		if item in prefs[p2]: 
-			si[item] = 1
-
-	
-	if len(si) == 0:
-		return 0
-
-	# sum calculations
-	n = len(si)
-
-	# sum of all preferences
-	sum1 = sum([prefs[p1][it] for it in si])
-	sum2 = sum([prefs[p2][it] for it in si])
-
-	# sum of the squares
-	sum1_sq = sum([pow(prefs[p1][it],2) for it in si])
-	sum2_sq = sum([pow(prefs[p2][it],2) for it in si])
-
-	# sum of the products
-	prod_sum = sum([prefs[p1][it] * prefs[p2][it] for it in si])
-
-	# calculate r (Pearson score)
-	num = (n*prod_sum) - (sum1 * sum2)
-	den = sqrt(((n*sum1_sq) - pow(sum1,2)) * (n*(sum2_sq) - pow(sum2,2)))
-	if den == 0:
-		return 0
-
-	r = num/den
-	return r
-
-
-
-'''
-returns the best matches for p from the prefs dictionary 
-'''
-def get_top_matches(prefs, p, similarity=sim_pearson, n=10):
-	scores = [(similarity(prefs,p,other),other)
-				for other in prefs if other != p]
-	scores.sort()
-	scores.reverse()
-	return scores
-
-
-
-
-
-def get_similar_items(prefs):
-	result = {}
-	# invert the preference matrix to be item-centric
-	item_prefs = transform_prefs(prefs)
-	for item in item_prefs:
-		scores = get_top_matches(item_prefs,item)
-		result[item] = scores
-	return result
-
-
-
-'''
-gets recommendations for a person by using a weighted average of every other user's rankings
-'''
-def get_user_based_recommendations(person, prefs, similarity=sim_pearson, n=10):
-	totals = {}
-	sim_sum = {}
-	#print person
-	#print prefs[person]
-	for other in prefs:
-		# don't compare to itself
-		if other == person:
-			continue
-		sim = similarity(prefs,person,other)
-		#print sim, person, other
-		# ignore non-corelated users
-		if sim <= 0: 
-			continue
-		for item in prefs[other]:
-			# only score items you haven't seen yet
-			if(item not in prefs[person]):
-				#print item
-				# similarity * score
-				totals.setdefault(item,0)
-				totals[item] += prefs[other][item] * sim
-				# sum of similarities
-				sim_sum.setdefault(item,0)
-				sim_sum[item] += sim
-
-	# the normalized list
-	normalized = {}
-	for item,total in totals.items():
-		normalized[item] = total/sim_sum[item]
-	
-	rankings = sorted(normalized.iteritems(), key=operator.itemgetter(1), reverse=True)
-	#print rankings
-	res = [{'item':get_string_id(item), 'score':score} for item,score in rankings]
-	return res[0:n]
-	
-
-
-'''
-gets recommendations for a person by using a weighted average of every other user's rankings
-'''
-def get_item_based_recommendations(person, prefs, similar_items, similarity=sim_pearson, n=10):
-	ratings = prefs[person]
-	totals = {}
-	sim_sum = {}
-
-	# loop over items rated by this user
-	for (item, rating) in ratings.items():
-		# loop over items similar to this one
-		for (similarity, item2) in similar_items[item]:
-			# ignore if this user has already rated this item
-			if item2 in ratings:
-				continue
-			if(similarity<0.3):
-				continue
-			# weighted sum of rating times similarity
-			totals.setdefault(item2,0)
-			totals[item2] += similarity * rating
-			#Sum of all the similarities
-			sim_sum.setdefault(item2,0)
-			sim_sum[item2]+=similarity
-
-	normalized = {}
-	for item,total in totals.items():
-		normalized[item] = total/sim_sum[item]
-
-	rankings = sorted(normalized.iteritems(), key=operator.itemgetter(1), reverse=True)
-	#print rankings
-	res = [{'item':get_string_id(item), 'score':score} for item,score in rankings]
-	return res[0:n]
-
-
-
-
-# transform Person, item - > Item, person
-def transform_prefs(prefs):
-	results = {}
-	for p in prefs:
-		for item in prefs[p]:
-			results.setdefault(item,{})
-
-			# flip item and person
-			results[item][p] = prefs[p][item]
-	return results
 
 class Recommender:
 	def __init__(self):
 		p = Prefs()
-		self.prefs = p.get_paper_prefs()
-		self.similar_items = get_similar_items(self.prefs)
+		self.gateway = JavaGateway()
 
 	def get_item_based_recommendations(self, paper_id):
-		return get_item_based_recommendations(get_long_id(paper_id), self.prefs, self.similar_items)
-
-	def get_user_based_recommendations(self, paper_id):
-		return get_user_based_recommendations(get_long_id(paper_id), self.prefs, self.similar_items)
-
+		recs = self.gateway.entry_point.recommend(str(encode_paper_id(paper_id)))
+		res=[]
+		for rec in recs:
+			r = rec.split(',')
+			res.append({'id': decode_paper_id(long(r[0])), 'score': float(r[1])})
+		return res
 
 
 def main():
--- db/prefs.py
+++ db/prefs.py
@@ -17,65 +17,65 @@ load prefs
 
 
 def encode_author_id(paper_id, authorId):
-		ret = 0
-		author_id = 0
-		if(authorId!=''):
-			author_id = int(authorId[4:])
-		if(paper_id.startswith('alt')):
-			ret= '1%05d%06d' %(int(paper_id[3:]), author_id)
-		elif(paper_id.startswith('case')):
-			ret= '2%05d%06d' %(int(paper_id[4:]), author_id)
-		elif(paper_id.startswith('crs')):
-			ret= '3%05d%06d' %(int(paper_id[3:]), author_id)
-		elif(paper_id.startswith('pan')):
-			ret= '4%05d%06d' %(int(paper_id[3:]), author_id)
-		elif(paper_id.startswith('pn')):
-			ret= '5%05d%06d' %(int(paper_id[2:]), author_id)
-		elif(paper_id.startswith('sig')):
-			ret= '6%05d%06d' %(int(paper_id[3:]), author_id)
-		elif(paper_id.startswith('tochi')):
-			ret= '7%05d%06d' %(int(paper_id[5:]), author_id)
-		return long(ret)
+	ret = 0
+	author_id = 0
+	if(authorId!=''):
+		author_id = int(authorId[4:])
+	if(paper_id.startswith('alt')):
+		ret= '1%05d%06d' %(int(paper_id[3:]), author_id)
+	elif(paper_id.startswith('case')):
+		ret= '2%05d%06d' %(int(paper_id[4:]), author_id)
+	elif(paper_id.startswith('crs')):
+		ret= '3%05d%06d' %(int(paper_id[3:]), author_id)
+	elif(paper_id.startswith('pan')):
+		ret= '4%05d%06d' %(int(paper_id[3:]), author_id)
+	elif(paper_id.startswith('pn')):
+		ret= '5%05d%06d' %(int(paper_id[2:]), author_id)
+	elif(paper_id.startswith('sig')):
+		ret= '6%05d%06d' %(int(paper_id[3:]), author_id)
+	elif(paper_id.startswith('tochi')):
+		ret= '7%05d%06d' %(int(paper_id[5:]), author_id)
+	return long(ret)
 
 
 def encode_paper_id(paper_id):
-		ret = 0
-		if(paper_id.startswith('alt')):
-			ret= '1%05d' %(int(paper_id[3:]))
-		elif(paper_id.startswith('case')):
-			ret= '2%05d' %(int(paper_id[4:]))
-		elif(paper_id.startswith('crs')):
-			ret= '3%05d' %(int(paper_id[3:]))
-		elif(paper_id.startswith('pan')):
-			ret= '4%05d' %(int(paper_id[3:]))
-		elif(paper_id.startswith('pn')):
-			ret= '5%05d' %(int(paper_id[2:]))
-		elif(paper_id.startswith('sig')):
-			ret= '6%05d' %(int(paper_id[3:]))
-		elif(paper_id.startswith('tochi')):
-			ret= '7%05d' %(int(paper_id[5:]))
-		return long(ret)
+	ret = ''
+	if(paper_id.startswith('alt')):
+		ret= '1%05d' %(long(paper_id[3:]))
+	elif(paper_id.startswith('case')):
+		ret= '2%05d' %(long(paper_id[4:]))
+	elif(paper_id.startswith('crs')):
+		ret= '3%05d' %(long(paper_id[3:]))
+	elif(paper_id.startswith('pan')):
+		ret= '4%05d' %(long(paper_id[3:]))
+	elif(paper_id.startswith('pn')):
+		ret= '5%05d' %(long(paper_id[2:]))
+	elif(paper_id.startswith('sig')):
+		ret= '6%05d' %(long(paper_id[3:]))
+	elif(paper_id.startswith('tochi')):
+		ret= '7%05d' %(long(paper_id[5:]))
+	return long(ret)
 
 def decode_paper_id(long_id):
-		str_id = str(long_id)
-		prefix = int(str_id[0:1])
-		paper_id_str = str_id[1:]
-		paper_id_pre= ''
-		if(prefix == 1):
-			paper_id_pre= 'alt'
-		elif(prefix == 2):
-			paper_id_pre= 'case'
-		elif(prefix == 3):
-			paper_id_pre= 'crs'
-		elif(prefix == 4):
-			paper_id_pre= 'pan'
-		elif(prefix == 5):
-			paper_id_pre= 'pn'
-		elif(prefix == 6):
-			paper_id_pre= 'sig'
-		elif(prefix == 7):
-			paper_id_pre= 'tochi'
-		return paper_id_pre+paper_id_str
+	str_id = str(long_id)
+	prefix = int(str_id[0:1])
+	paper_id_str = str(int(str_id[1:]))
+	paper_id_pre= ''
+	if(prefix == 1):
+		paper_id_pre= 'alt'
+	elif(prefix == 2):
+		paper_id_pre= 'case'
+	elif(prefix == 3):
+		paper_id_pre= 'crs'
+	elif(prefix == 4):
+		paper_id_pre= 'pan'
+	elif(prefix == 5):
+		paper_id_pre= 'pn'
+	elif(prefix == 6):
+		paper_id_pre= 'sig'
+	elif(prefix == 7):
+		paper_id_pre= 'tochi'
+	return paper_id_pre+paper_id_str
 
 class Prefs:
 
