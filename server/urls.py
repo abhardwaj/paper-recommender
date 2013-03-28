29b7643b4e42e63db915619b88a9dd40df91044a
Anant Bhardwaj <anantb@csail.mit.edu>
2013-03-27 22:26:49 -0400

    add user segment

--- server/urls.py
+++ server/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
 	url(r'^$','server.views.index'),
 	url(r'^index', 'server.views.index'),
-    url(r'^recommend', 'server.views.recommend'),
-    url(r'^paper/(\w+)$', 'server.views.similar_papers'),
+    url(r'^user/(\w+)$', 'server.views.user'),
+    url(r'^paper/(\w+)$', 'server.views.paper'),
 
     url(r'^login', 'server.views.login'),
 	url(r'^logout', 'server.views.logout'),
--- server/views.py
+++ server/views.py
@@ -76,13 +76,39 @@ def index(request):
 			l['title']= e.entities[id]['title']
 			likes.append(l)
 		#print recs
-		return render_to_response("index.html", {'user': user, 'recs':recs, 'likes':likes})
+		return render_to_response("index.html", {'login': user, 'recs':recs, 'likes':likes})
 	except KeyError:
 		return HttpResponseRedirect('login')
 
 
-	
-def similar_papers(request, paper_id):	
+def user(request, author_id):
+	try:
+		user = author_id
+		login = request.session[SESSION_KEY]
+		recs = []
+		likes = []
+		papers_liked = p.author_likes[user]['likes']
+		items = r.get_item_based_recommendations(papers_liked)
+		for item in items:
+			rec = {}
+			id=item['id']
+			rec['id']=id
+			rec['title']= e.entities[id]['title']
+			recs.append(rec)
+		for like in papers_liked:
+			l = {}
+			id=like
+			l['id']=id
+			l['title']= e.entities[id]['title']
+			likes.append(l)
+		#print recs
+		return render_to_response("user.html", {'login': login, 'user': user, 'recs':recs, 'likes':likes})
+	except KeyError:
+		return HttpResponseRedirect('login')	
+
+
+
+def paper(request, paper_id):	
 	items = r.get_item_based_recommendations([paper_id])
 	user = request.session[SESSION_KEY]
 	recs = []
@@ -93,7 +119,7 @@ def similar_papers(request, paper_id):
 		rec['id']=id
 		rec['title']= e.entities[id]['title']
 		recs.append(rec)
-	return render_to_response("paper.html", {'user': user, 'id':paper_id, 'title': paper_title, 'recs':recs})
+	return render_to_response("paper.html", {'login': user, 'id':paper_id, 'title': paper_title, 'recs':recs})
 
 
 
