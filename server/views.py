5c53df14ccbd380a1374e210dabfae627c4e8157
Anant Bhardwaj <anantb@csail.mit.edu>
2013-04-10 00:08:55 -0400

    PCS Login

--- server/views.py
+++ server/views.py
@@ -25,7 +25,7 @@ s = Session()
 
 
 
-SESSION_KEY = 'USER'
+
 
 def init_session(email):
 	pass
@@ -43,14 +43,22 @@ def login_form(request):
 def login(request, redirect_url='index'):
 	if request.method == "POST":
 		try:
-			user = request.POST["login_author"]
-			if(user != ""):
+			login_email = request.POST["login_email"]
+
+			if(login_email != ""):
 				request.session.flush()
-				request.session[SESSION_KEY] = user
+				cursor = connection.cursor()
+				cursor.execute("""SELECT id, given_name, family_name from pcs_authors where email1 like '%s' or 
+					email2 like '%s' or email3 like '%s';""" %(login_email, login_email, login_email))
+				data = cursor.fetchall()
+				request.session['id'] = data[0][0]
+				request.session['email'] = login_email
+				request.session['name'] = data[0][1] + ' ' + data[0][2]
 				return HttpResponseRedirect(redirect_url)
 			else:
 				return login_form(request)
 		except:
+			print sys.exc_info()
 			return login_form(request)
 	else:
 		return login_form(request)
@@ -65,41 +73,43 @@ def logout(request):
 
 def index(request):
 	try:
-		user = request.session[SESSION_KEY]
+		user = request.session['id']
 		recs = []
 		likes = []
-		papers_liked = p.author_likes[user]['likes']
-		items = r.get_item_based_recommendations(papers_liked)
-		for item in items:
-			rec = {}
-			id=item['id']
-			rec['id']=id
-			rec.update(e.entities[id])
-			recs.append(rec)
-		for like in papers_liked:
-			l = {}
-			id=like
-			l['id']=id
-			l.update(e.entities[id])
-			likes.append(l)
+		if(user in p.author_likes):
+			papers_liked = p.author_likes[user]['likes']
+			items = r.get_item_based_recommendations(papers_liked)
+			for item in items:
+				rec = {}
+				id=item['id']
+				rec['id']=id
+				rec.update(e.entities[id])
+				recs.append(rec)
+			for like in papers_liked:
+				l = {}
+				id=like
+				l['id']=id
+				l.update(e.entities[id])
+				likes.append(l)
 		#print recs
-		return render_to_response("index.html", {'login': p.author_likes[user]['name'], 'recs':recs, 'user_likes':likes})
+		return render_to_response("index.html", {'login': request.session['name'], 'recs':recs, 'user_likes':likes})
 	except KeyError:
+		print sys.exc_info()
 		return HttpResponseRedirect('login')
 
 
 def users(request):	
-	user = request.session[SESSION_KEY]
+	user = request.session['id']
 	users = []
 	for u in p.author_likes:
 		users.append({'id':u, 'name': p.author_likes[u]['name']})
-	return render_to_response("users.html", {'login': p.author_likes[user]['name'], 'users':users})
+	return render_to_response("users.html", {'login': request.session['name'], 'users':users})
 
 
 def user(request, author_id):
 	try:
 		user = author_id
-		login = request.session[SESSION_KEY]
+		login = request.session['id']
 		recs = []
 		likes = []
 		papers_liked = p.author_likes[user]['likes']
@@ -117,7 +127,7 @@ def user(request, author_id):
 			l.update(e.entities[id])
 			likes.append(l)
 		#print recs
-		return render_to_response("user.html", {'login': p.author_likes[login]['name'], 'user': p.author_likes[user]['name'], 'recs':recs, 'user_likes':likes, 'likes':p.author_likes[login]['likes']})
+		return render_to_response("user.html", {'login': request.session['name'], 'user': p.author_likes[user]['name'], 'recs':recs, 'user_likes':likes, 'likes':p.author_likes[login]['likes']})
 	except KeyError:
 		return HttpResponseRedirect('login')	
 
@@ -125,7 +135,7 @@ def user(request, author_id):
 
 def paper(request, paper_id):	
 	items = r.get_item_based_recommendations([paper_id])
-	user = request.session[SESSION_KEY]
+	user = request.session['id']
 	recs = []
 	paper = e.entities[paper_id]
 	for item in items:
@@ -134,12 +144,12 @@ def paper(request, paper_id):
 		rec['id']=id
 		rec.update(e.entities[id])
 		recs.append(rec)
-	return render_to_response("paper.html", {'login': p.author_likes[user]['name'], 'id':paper_id, 'paper': paper, 'recs':recs, 'likes':p.author_likes[user]['likes']})
+	return render_to_response("paper.html", {'login': request.session['name'], 'id':paper_id, 'paper': paper, 'recs':recs, 'likes':p.author_likes[user]['likes']})
 
 
 
 def like(request, paper_id, like_str):	
-	user = request.session[SESSION_KEY]
+	user = request.session['id']
 	if(like_str=='like' and (paper_id not in p.author_likes[user]['likes'])):
 		p.author_likes[user]['likes'].append(paper_id)
 		return HttpResponse(json.dumps({'status':'ok'}), mimetype="application/json")
@@ -151,10 +161,10 @@ def like(request, paper_id, like_str):
 
 
 def schedule(request):	
-	user = request.session[SESSION_KEY]
+	user = request.session['id']
 	sessions = s.sessions
 	for session in sessions:
 		for submission in sessions[session]['submissions']:
 			sessions[session]['submissions'][submission].update(e.entities[submission])
-	return render_to_response("schedule.html", {'login': p.author_likes[user]['name'], 'sessions':sessions, 'likes':p.author_likes[user]['likes']})
+	return render_to_response("schedule.html", {'login': request.session['name'], 'sessions':sessions, 'likes':p.author_likes[user]['likes']})
 
