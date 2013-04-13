eb00a4ed7b2f0547df1018b37c2dd982bfdf56cc
Anant Bhardwaj <anantb@csail.mit.edu>
2013-04-12 23:40:27 -0400

    more stuff

--- server/views.py
+++ server/views.py
@@ -32,7 +32,7 @@ def login_form(request):
 	c.update(csrf(request))
 	return render_to_response('login.html', c)
 
-def login(request, redirect_url='index'):
+def login(request, redirect_url='mobile'):
 	if request.method == "POST":
 		try:
 			login_email = request.POST["login_email"]
