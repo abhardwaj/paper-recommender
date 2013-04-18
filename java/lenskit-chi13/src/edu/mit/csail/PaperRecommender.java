2c56181e411e4488a03b650f1b5d7ca70ea5e98f
Anant Bhardwaj <anantb@csail.mit.edu>
2013-04-18 19:35:02 -0400

    get 100 recs

--- java/lenskit-chi13/src/edu/mit/csail/PaperRecommender.java
+++ java/lenskit-chi13/src/edu/mit/csail/PaperRecommender.java
@@ -48,12 +48,12 @@ public class PaperRecommender {
           input_items.add(Long.valueOf(i)); 
         }
 		ArrayList<String> ret = new ArrayList<String>();
-		ScoredLongList recommendations = this.grec.globalRecommend(input_items, 200);
-		long items[] = new long[200];
-		double scores[] = new double[200];
+		ScoredLongList recommendations = this.grec.globalRecommend(input_items, 100);
+		long items[] = new long[100];
+		double scores[] = new double[100];
 		if(recommendations.size() > 0)
-			recommendations.getElements(0, items, scores, 0, 200);
-		for(int i=0; i< 200; i++){
+			recommendations.getElements(0, items, scores, 0, 100);
+		for(int i=0; i< 100; i++){
 			ret.add(items[i] + "," + scores[i]);
 		}
 		return ret;
