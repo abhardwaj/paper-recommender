e1c829f8446c6f8074d8a8acca85adc98c083744
Anant Bhardwaj <anantb@csail.mit.edu>
2013-04-20 20:28:03 -0400

    scale = 1

--- server/static/javascript/desktop.js
+++ server/static/javascript/desktop.js
@@ -916,36 +916,3 @@ function enable_alert(msg){
     $("body").removeClass("notice");
   }, 5000);
 }
-
-
-
-(function($){
-  $.fn.extend({ 
-    clickTouch: function(handler) {
-       return this.each(function() {
-         var touchedWithoutScroll = false;
-         var self = $(this);
- 
-         self.bind('touchstart', function(event) {
-           // Ignore multi-touches
-           if(event.originalEvent.touches.length > 1) {
-             return;
-           }
-           touchedWithoutScroll = true;
-         })
-         // If user starts scrolling/panning, let the touch through
-         .bind('touchmove', function(event) {
-           touchedWithoutScroll = false;
-         })
-         // If user releases without scrolling/panning/multitouching, it's a touch
-         .bind('touchend', function(event) {
-           if(touchedWithoutScroll) {
-             handler.apply(this, arguments);
-             return false;
-           }
-         })
-         .click(handler);
-       });
-    }
-  });
-})(jQuery);
