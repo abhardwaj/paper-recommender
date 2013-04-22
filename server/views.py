269b707871575d947621cb3fb12dde02ebebe7a3
Anant Bhardwaj <anantb@csail.mit.edu>
2013-04-22 16:21:15 -0400

    email formatting

--- server/views.py
+++ server/views.py
@@ -50,7 +50,7 @@ def send_email(addr, id):
 	""" %(addr, id)
 	
 	msg = MIMEMultipart()
-	msg['From'] = 'mychi@csail.mit.edu <myCHI>'
+	msg['From'] = 'myCHI <mychi@csail.mit.edu>'
 	msg['To'] = ",".join(to_addr)
 	msg['Subject'] = email_subject
 	msg.attach(MIMEText(msg_body))	
