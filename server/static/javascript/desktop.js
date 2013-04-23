bdcddaa22b0e2d81d017cf51969b257fb742b1e2
Anant Bhardwaj <anantb@csail.mit.edu>
2013-04-23 05:09:57 -0400

    iPad

--- server/static/javascript/desktop.js
+++ server/static/javascript/desktop.js
@@ -58,11 +58,11 @@ var codeBlackList = [
   "YYP","NFM","PTT","NCK","PQG","KCL"
 ];
 
+//|| navigator.userAgent.match(/iPad/i)
 function detect_mobile() { 
  if(navigator.userAgent.match(/Android/i)
  || navigator.userAgent.match(/webOS/i)
  || navigator.userAgent.match(/iPhone/i)
- || navigator.userAgent.match(/iPad/i)
  || navigator.userAgent.match(/iPod/i)
  || navigator.userAgent.match(/BlackBerry/i)
  || navigator.userAgent.match(/Windows Phone/i)
